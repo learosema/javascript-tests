c41907be9f43ab0a1e2207883eb72a0b5ec6fb9a
Lea Rosema <terabaud@gmail.com>
2018-06-28 20:07:45 +0200

    Updates

--- index.js
+++
@@ -1,5 +0,0 @@
-import { add, mul, div } from './math.js'
-
-console.log(`2 + 2 = ${add(2, 2)}`)
-console.log(`2 * 2 = ${mul(2, 2)}`)
-console.log(`2 / 2 = ${div(2, 2)}`)
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import { add, mul, div } from './math.mjs'
+
+console.log(`2 + 2 = ${add(2, 2)}`)
+console.log(`2 * 3 = ${mul(2, 3)}`)
+console.log(`4 / 2 = ${div(4, 2)}`)
