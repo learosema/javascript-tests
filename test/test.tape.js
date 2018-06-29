2270f6624af0f202e12fe04561492965413fe91c
Lea Rosema <terabaud@gmail.com>
2018-06-29 19:54:47 +0200

    simplified

--- test/test.tape.js
+++ test/test.tape.js
@@ -1,17 +1,17 @@
 import { test } from 'tape'
 import { add, mul, div } from '../src/math'
 
-test('add(1, 2) returns 3', function ({ plan, equal }) {
-  plan(1)
+test('add(1, 2) returns 3', ({ end, equal }) => {
   equal(add(1, 2), 3)
+  end()
 })
 
-test('mul(3, 4) returns 12', function ({ plan, equal }) {
-  plan(1)
+test('mul(3, 4) returns 12', ({ end, equal }) => {
   equal(mul(3, 4), 12)
+  end()
 })
 
-test('div(20, 2) returns 10', function ({ plan, equal }) {
-  plan(1)
+test('div(20, 2) returns 10', ({ end, equal }) => {
   equal(div(20, 2), 10)
+  end()
 })
